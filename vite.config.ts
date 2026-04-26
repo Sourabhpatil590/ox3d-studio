82f7ac5ef6594ec9ea84f8db2fb324401882ddc5
Sourabhpatil590 <128510300+Sourabhpatil590@users.noreply.github.com>
2026-04-27 01:25:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart(),
+    tanstackStart({
+      target: 'static',  // add this
+    }),
     react(),
   ],
 })
