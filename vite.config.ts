fc6c2f9406dd1cc39f224265e148998c3aee7c0f
Sourabhpatil590 <128510300+Sourabhpatil590@users.noreply.github.com>
2026-04-27 01:53:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart({
-      target: 'vercel',
-    }),
+    tanstackStart(),
+    nitro(),
     react(),
   ],
 })
